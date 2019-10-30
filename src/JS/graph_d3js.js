bd9d9e7ef17501b310820a2d808368228c3ed5af
Youmustkidding <32686212+Youmustkidding@users.noreply.github.com>
2019-10-30 18:31:11 +0100

    Split script in function + freeze on F

--- src/JS/graph_d3js.js
+++ src/JS/graph_d3js.js
@@ -1,207 +1,129 @@
-window.onload = function () {
-    //Keyboard Event
-    d3.select("body").on("keydown", function () {
-        //Suppr
-        if (d3.event.keyCode == 46) {
-            if (currentObject != null) {
-                RemoveElement(currentObject);
-            }
-        };
-    })
+//The graph properties
+var graphJSON;
+var force;
+var color;
+var width;
+var height;
+var drag_in_progress = false;
+var is_freeze = false;
 
-    var pos;
+//DOM Elements / D3JS Elements
+var nodes, links, loops, v_labels, e_labels, l_labels,line,svg;
 
-    // Loads the graph data
+// Loads the graph data
+function LoadGraphData() {
     var mydiv = document.getElementById("mygraph")
     var graph_as_string = mydiv.innerHTML
-    var graph = eval('(' + graph_as_string + ')');
+    graphJSON = eval('(' + graph_as_string + ')');
 
-    var width = document.documentElement.clientWidth - 32;
-    var height = document.documentElement.clientHeight - 32;
-
+    width = document.documentElement.clientWidth - 32;
+    height = document.documentElement.clientHeight - 32;
     // List of colors
-    var color = d3.scale.category10();
+    color = d3.scale.category10();
+}
 
-    var force = d3.layout.force()
-        .charge(graph.charge)
-        .linkDistance(graph.link_distance)
-        .linkStrength(graph.link_strength)
-        .gravity(graph.gravity)
+function InitGraph() {
+    force = d3.layout.force()
+        .charge(graphJSON.charge)
+        .linkDistance(graphJSON.link_distance)
+        .linkStrength(graphJSON.link_strength)
+        .gravity(graphJSON.gravity)
         .size([width, height])
-        .links(graph.links)
-        .nodes(graph.nodes);
+        .links(graphJSON.links)
+        .nodes(graphJSON.nodes);
 
     // Adapts the graph layout to the javascript window's dimensions
-    if (graph.pos.length != 0) {
-        center_and_scale(graph);
+    if (graphJSON.pos.length != 0) {
+        center_and_scale();
     }
-    // SVG window
-    var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("pointer-events", "all") // Zoom+move management
-        .append('svg:g')
-        .call(d3.behavior.zoom().on("zoom", redraw_on_zoom))
-        .append('svg:g');
+}
 
-    // Zooming
-    svg.append('svg:rect')
-        .attr('x', -10000)
-        .attr('y', -10000)
-        .attr('width', 2 * 10000)
-        .attr('height', 2 * 10000);
-
-    var drag_in_progress = false;
-
-    function redraw_on_zoom() {
-        if (!drag_in_progress) {
-            svg.attr("transform",
-                "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")");
+function KeyboardEventInit() {
+    //Keyboard Event
+    d3.select("body").on("keydown", function () {
+        switch (d3.event.keyCode) {
+            //Suppr
+            case 46:
+                if (currentObject != null) {
+                    RemoveElement(currentObject);
+                }
+                break;
+            //F for Freeze
+            case 70:
+                FreezeGraph();
+                break;
+            //T for Test, to remove before build
+            case 84: 
+                console.log("Test");
+                break;
+            default:
+                //Affiche le code de la touche pressée
+                console.log("Keycode : " + d3.event.keyCode);
+                break;
         }
+    })
+}
+
+function redraw_on_zoom() {
+    if (!drag_in_progress) {
+        svg.attr("transform",
+            "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")");
     }
-
-    // Edges
-    var link = svg.selectAll(".link")
-        .data(force.links())
-        .enter().append("path")
-        .attr("class", function (d) {
-            return "link directed";
-        })
-        .attr("id", function (current) {
-            return current.source + "_" + current.target;
-        })
-        .attr("marker-end", function (d) {
-            return "url(#directed)";
-        })
-        .on("mouseover", function () {
-            currentObject = this;
-        })
-        .on("mouseout", function () {
-            currentObject = null;
-        })
-        .style("stroke", function (d) {
-            return d.color;
-        })
-        .style("stroke-width", graph.edge_thickness + "px");
-
-    // Loops
-    var loops = svg.selectAll(".loop")
-        .data(graph.loops)
-        .enter().append("circle")
-        .attr("class", "link")
-        .attr("r", function (d) {
-            return d.curve;
-        })
-        .style("stroke", function (d) {
-            return d.color;
-        })
-        .style("stroke-width", graph.edge_thickness + "px");
+}
 
 
-    // Nodes
-    var node = svg.selectAll(".node")
-        .data(force.nodes())
-        .enter().append("circle")
-        .attr("class", "node")
-        .attr("r", graph.vertex_size)
-        .attr("id", function (d) {
-            return d.name;
-        })
-        .style("fill", function (d) {
-            return color(d.group);
-        })
-        .on("mouseover", function () {
-            currentObject = this;
-        })
-        .on("mouseout", function () {
-            currentObject = null;
-        })
-        .call(force.drag()
-            .on('dragstart', function () {
-                drag_in_progress = true;
-            })
-            .on('dragend', function () {
-                drag_in_progress = false;
-            }))
+// Returns the coordinates of a point located at distance d from the
+// barycenter of two points pa, pb.
+function third_point_of_curved_edge(pa, pb, d) {
+    var ox = pa.x,
+        oy = pa.y,
+        dx = pb.x,
+        dy = pb.y;
+    var cx = (dx + ox) / 2,
+        cy = (dy + oy) / 2;
+    var ny = -(dx - ox),
+        nx = dy - oy;
+    var nn = Math.sqrt(nx * nx + ny * ny)
+    return [cx + d * nx / nn, cy + d * ny / nn]
+}
 
-    node.append("title").text(function (d) {
-        return d.name;
+// Applies an homothety to the points of the graph respecting the
+// aspect ratio, so that the graph takes the whole javascript
+// window and is centered
+function center_and_scale() {
+    var minx = graphJSON.pos[0][0];
+    var maxx = graphJSON.pos[0][0];
+    var miny = graphJSON.pos[0][1];
+    var maxy = graphJSON.pos[0][1];
+
+    graphJSON.nodes.forEach(function (d,i) {
+        maxx = Math.max(maxx, graphJSON.pos[i][0]);
+        minx = Math.min(minx, graphJSON.pos[i][0]);
+        maxy = Math.max(maxy, graphJSON.pos[i][1]);
+        miny = Math.min(miny, graphJSON.pos[i][1]);
     });
 
-    // Vertex labels
-    if (graph.vertex_labels) {
-        var v_labels = svg.selectAll(".v_label")
-            .data(force.nodes())
-            .enter()
-            .append("svg:text")
-            .attr("vertical-align", "middle")
-            .attr("id", function (d) {
-                return d.name + "label";
-            })
-            .text(function (d) {
-                return d.name;
-            })
-    }
-    // Edge labels
-    if (graph.edge_labels) {
-        var e_labels = svg.selectAll(".e_label")
-            .data(force.links())
-            .enter()
-            .append("svg:text")
-            .attr("text-anchor", "middle")
-            .text(function (d) {
-                return d.name;
-            })
+    var border = 60
+    var xspan = maxx - minx;
+    var yspan = maxy - miny;
 
-        var l_labels = svg.selectAll(".l_label")
-            .data(graph.loops)
-            .enter()
-            .append("svg:text")
-            .attr("text-anchor", "middle")
-            .text(function (d, i) {
-                return graph.loops[i].name;
-            })
-    }
+    var scale = Math.min((height - border) / yspan, (width - border) / xspan);
+    var xshift = (width - scale * xspan) / 2
+    var yshift = (height - scale * yspan) / 2
 
-    // Arrows, for directed graphs
-    if (graph.directed) {
-        svg.append("svg:defs").selectAll("marker")
-            .data(["directed"])
-            .enter().append("svg:marker")
-            .attr("id", String)
-            // viewbox is a rectangle with bottom-left corder (0,-2), width 4 and height 4
-            .attr("viewBox", "0 -2 4 4")
-            // This formula took some time ... :-P
-            .attr("refX", Math.ceil(2 * Math.sqrt(graph.vertex_size)))
-            .attr("refY", 0)
-            .attr("markerWidth", 4)
-            .attr("markerHeight", 4)
-            .attr("preserveAspectRatio", false)
-            .attr("orient", "auto")
-            .append("svg:path")
-            // triangles with endpoints (0,-2), (4,0), (0,2)
-            .attr("d", "M0,-2L4,0L0,2");
-    }
+    force.nodes().forEach(function (d, i) {
+        d.x = scale * (graphJSON.pos[i][0] - minx) + xshift;
+        d.y = scale * (graphJSON.pos[i][1] - miny) + yshift;
+    });
+}
 
-    // The function 'line' takes as input a sequence of tuples, and returns a
-    // curve interpolating these points.
-    var line = d3.svg.line()
-        .interpolate("cardinal")
-        .tension(.2)
-        .x(function (d) {
-            return d.x;
-        })
-        .y(function (d) {
-            return d.y;
-        })
-
-    /////////////////////////////////////////////
-    // This is where all movements are defined //
-    /////////////////////////////////////////////
+//Define all forces movements
+function InitForce()
+{
     force.on("tick", function () {
 
         // Position of vertices
-        node.attr("cx", function (d) {
+        nodes.attr("cx", function (d) {
                 return d.x;
             })
             .attr("cy", function (d) {
@@ -209,7 +131,7 @@ window.onload = function () {
             });
 
         // Position of edges
-        link.attr("d", function (d) {
+        links.attr("d", function (d) {
 
             // Straight edges
             if (d.curve == 0) {
@@ -235,7 +157,7 @@ window.onload = function () {
         });
 
         // Position of Loops
-        if (graph.loops.length != 0) {
+        if (graphJSON.loops.length != 0) {
             loops
                 .attr("cx", function (d) {
                     return force.nodes()[d.source].x;
@@ -246,17 +168,17 @@ window.onload = function () {
         }
 
         // Position of vertex labels
-        if (graph.vertex_labels) {
+        if (graphJSON.vertex_labels) {
             v_labels
                 .attr("x", function (d) {
-                    return d.x + graph.vertex_size;
+                    return d.x + graphJSON.vertex_size;
                 })
                 .attr("y", function (d) {
                     return d.y;
                 })
         }
         // Position of the edge labels
-        if (graph.edge_labels) {
+        if (graphJSON.edge_labels) {
             e_labels
                 .attr("x", function (d) {
                     return third_point_of_curved_edge(d.source, d.target, d.curve + 3)[0];
@@ -273,62 +195,189 @@ window.onload = function () {
                 })
         }
     });
+}
 
-    // Returns the coordinates of a point located at distance d from the
-    // barycenter of two points pa, pb.
-    function third_point_of_curved_edge(pa, pb, d) {
-        var ox = pa.x,
-            oy = pa.y,
-            dx = pb.x,
-            dy = pb.y;
-        var cx = (dx + ox) / 2,
-            cy = (dy + oy) / 2;
-        var ny = -(dx - ox),
-            nx = dy - oy;
-        var nn = Math.sqrt(nx * nx + ny * ny)
-        return [cx + d * nx / nn, cy + d * ny / nn]
+function AppendAllGraphElement()
+{
+    // SVG window
+    svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("pointer-events", "all") // Zoom+move management
+        .append('svg:g')
+        .call(d3.behavior.zoom().on("zoom", redraw_on_zoom))
+        .append('svg:g');
+
+    // Zooming
+    svg.append('svg:rect')
+        .attr('x', -10000)
+        .attr('y', -10000)
+        .attr('width', 2 * 10000)
+        .attr('height', 2 * 10000);
+
+
+
+    // Edges
+    links = svg.selectAll(".link")
+        .data(force.links())
+        .enter().append("path")
+        .attr("class", function (d) {
+            return "link directed";
+        })
+        .attr("id", function (current) {
+            return current.source + "_" + current.target;
+        })
+        .attr("marker-end", function (d) {
+            return "url(#directed)";
+        })
+        .on("mouseover", function () {
+            currentObject = this;
+        })
+        .on("mouseout", function () {
+            currentObject = null;
+        })
+        .style("stroke", function (d) {
+            return d.color;
+        })
+        .style("stroke-width", graphJSON.edge_thickness + "px");
+
+    // Loops
+    loops = svg.selectAll(".loop")
+        .data(graphJSON.loops)
+        .enter().append("circle")
+        .attr("class", "link")
+        .attr("r", function (d) {
+            return d.curve;
+        })
+        .style("stroke", function (d) {
+            return d.color;
+        })
+        .style("stroke-width", graphJSON.edge_thickness + "px");
+
+
+    AppendNodesElement(force.nodes());
+
+    // Vertex labels
+    if (graphJSON.vertex_labels) {
+        v_labels = svg.selectAll(".v_label")
+            .data(force.nodes())
+            .enter()
+            .append("svg:text")
+            .attr("vertical-align", "middle")
+            .attr("id", function (d) {
+                return d.name + "label";
+            })
+            .text(function (d) {
+                return d.name;
+            })
     }
 
-    // Applies an homothety to the points of the graph respecting the
-    // aspect ratio, so that the graph takes the whole javascript
-    // window and is centered
-    function center_and_scale(graph) {
-        var minx = graph.pos[0][0];
-        var maxx = graph.pos[0][0];
-        var miny = graph.pos[0][1];
-        var maxy = graph.pos[0][1];
+    // Edge labels
+    if (graphJSON.edge_labels) {
+        e_labels = svg.selectAll(".e_label")
+            .data(force.links())
+            .enter()
+            .append("svg:text")
+            .attr("text-anchor", "middle")
+            .text(function (d) {
+                return d.name;
+            })
 
-        graph.nodes.forEach(function (d, i) {
-            maxx = Math.max(maxx, graph.pos[i][0]);
-            minx = Math.min(minx, graph.pos[i][0]);
-            maxy = Math.max(maxy, graph.pos[i][1]);
-            miny = Math.min(miny, graph.pos[i][1]);
-        });
-
-        var border = 60
-        var xspan = maxx - minx;
-        var yspan = maxy - miny;
-
-        var scale = Math.min((height - border) / yspan, (width - border) / xspan);
-        var xshift = (width - scale * xspan) / 2
-        var yshift = (height - scale * yspan) / 2
-
-        force.nodes().forEach(function (d, i) {
-            d.x = scale * (graph.pos[i][0] - minx) + xshift;
-            d.y = scale * (graph.pos[i][1] - miny) + yshift;
-        });
+        l_labels = svg.selectAll(".l_label")
+            .data(graphJSON.loops)
+            .enter()
+            .append("svg:text")
+            .attr("text-anchor", "middle")
+            .text(function (d, i) {
+                return graphJSON.loops[i].name;
+            })
     }
 
+    // Arrows, for directed graphs
+    if (graphJSON.directed) {
+        svg.append("svg:defs").selectAll("marker")
+            .data(["directed"])
+            .enter().append("svg:marker")
+            .attr("id", String)
+            // viewbox is a rectangle with bottom-left corder (0,-2), width 4 and height 4
+            .attr("viewBox", "0 -2 4 4")
+            // This formula took some time ... :-P
+            .attr("refX", Math.ceil(2 * Math.sqrt(graphJSON.vertex_size)))
+            .attr("refY", 0)
+            .attr("markerWidth", 4)
+            .attr("markerHeight", 4)
+            .attr("preserveAspectRatio", false)
+            .attr("orient", "auto")
+            .append("svg:path")
+            // triangles with endpoints (0,-2), (4,0), (0,2)
+            .attr("d", "M0,-2L4,0L0,2");
+    }
+
+    // The function 'line' takes as input a sequence of tuples, and returns a
+    // curve interpolating these points.
+    line = d3.svg.line()
+        .interpolate("cardinal")
+        .tension(.2)
+        .x(function (d) {
+            return d.x;
+        })
+        .y(function (d) {
+            return d.y;
+        })
+
+}
+
+//Enable or disable the forces
+function FreezeGraph() {
+    is_freeze = !is_freeze;
+
+    graphJSON.nodes.forEach(function (d) {
+        d.fixed = is_freeze;
+    });
+}
+
+function AppendNodesElement(nodesData)
+{
+    // Nodes
+    nodes = svg.selectAll(".node")
+    .data(nodesData)
+    .enter().append("circle")
+    .attr("class", "node")
+    .attr("r", graphJSON.vertex_size)
+    .attr("id", function (d) {
+        return d.name;
+    })
+    .style("fill", function (d) {
+        return color(d.group);
+    })
+    .on("mouseover", function () {
+        currentObject = this;
+    })
+    .on("mouseout", function () {
+        currentObject = null;
+    })
+    .call(force.drag()
+        .on('dragstart', function () {
+            drag_in_progress = true;
+        })
+        .on('dragend', function () {
+            drag_in_progress = false;
+        }))
+
+    nodes.append("title").text(function (d) {
+    return d.name;
+    });
+}
+
+window.onload = function () {
+
+    LoadGraphData();
+    InitGraph();
+    KeyboardEventInit();
+
+    AppendAllGraphElement();
+
+    InitForce();
     // Starts the automatic force layout
-
     force.start();
-
-    if (graph.pos.length != 0) {
-        force.tick();
-        force.stop();
-        graph.nodes.forEach(function (d, i) {
-            d.fixed = true;
-        });
-    }
-
 }
