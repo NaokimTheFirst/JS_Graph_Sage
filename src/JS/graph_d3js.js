3bb5fa948a6f2d896358abb0492454cac6e016f8
Youmustkidding <32686212+Youmustkidding@users.noreply.github.com>
2020-02-19 17:04:08 +0100

    Better selection of the edges

--- src/JS/graph_d3js.js
+++ src/JS/graph_d3js.js
@@ -26,6 +26,21 @@ const LoopType = "loop";
 const NodeType = "Node";
 const EdgeType = "link directed";
 
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Segment {
+    constructor(start, end) {
+        this.start = start;
+        this.end = end;
+    }
+}
+
+
 class ValueRegisterer {
     constructor(oldValue, newValue, element) {
         this.oldValue = oldValue;
@@ -345,11 +360,119 @@ function SelectElementsInsideExtent(extent) {
         }
     })
     links.each(function (d) {
-        if (IsNodeInsideExtent(extent, d.source) && IsNodeInsideExtent(extent, d.target))
-        {
+        if(IsEdgeInsideExtent(extent,d)){
             SelectElement(new Element(d,EdgeType));
         }
     })
+}
+
+function ConstructRectangleFromExtent(extent)
+{
+    topLeftCorner = new Point(extent[0][0],extent[0][1]);
+    topRightCorner = new Point(extent[1][0],extent[0][1]);
+    bottomLefttCorner = new Point(extent[0][0],extent[1][1]);
+    bottomRightCorner = new Point(extent[1][0],extent[1][1]);
+
+    topBorder = new Segment(topLeftCorner,topRightCorner);
+    leftBorder = new Segment(topLeftCorner, bottomLefttCorner);
+    rightBorder = new Segment(topRightCorner, bottomRightCorner);
+    bottomBorder = new Segment(bottomRightCorner, bottomLefttCorner);
+
+    rectangle = [topBorder,leftBorder,rightBorder,bottomBorder];
+
+    return rectangle;
+}
+
+// Given three colinear points p, q, r, the function checks if 
+// point q lies on line segment 'pr' 
+function onSegment( p,  q,  r) 
+{ 
+    if (q.x <= Math.max(p.x, r.x) && q.x >= Math.min(p.x, r.x) && 
+        q.y <= Math.max(p.y, r.y) && q.y >= Math.min(p.y, r.y)) 
+    return true; 
+
+    return false; 
+} 
+
+// To find orientation of ordered triplet (p, q, r). 
+// The function returns following values 
+// 0 --> p, q and r are colinear 
+// 1 --> Clockwise 
+// 2 --> Counterclockwise 
+function orientation(p,  q,  r) 
+{ 
+    // See https://www.geeksforgeeks.org/orientation-3-ordered-points/ 
+    // for details of below formula. 
+        val = (q.y - p.y) * (r.x - q.x) - 
+            (q.x - p.x) * (r.y - q.y); 
+
+    if (val == 0) return 0; // colinear 
+
+    return (val > 0)? 1: 2; // clock or counterclock wise 
+} 
+
+function doSegmentIntersect(firstSegment, secondSegment) 
+{
+    return doIntersect(firstSegment.start,firstSegment.end,secondSegment.start,secondSegment.end);
+}
+
+// The main function that returns true if line segment 'p1q1' 
+// and 'p2q2' intersect. 
+function doIntersect( p1,  q1,  p2,  q2) 
+{ 
+    // Find the four orientations needed for general and 
+    // special cases 
+        o1 = orientation(p1, q1, p2); 
+        o2 = orientation(p1, q1, q2); 
+        o3 = orientation(p2, q2, p1); 
+        o4 = orientation(p2, q2, q1); 
+
+    // General case 
+    if (o1 != o2 && o3 != o4) 
+        return true; 
+
+    // Special Cases 
+    // p1, q1 and p2 are colinear and p2 lies on segment p1q1 
+    if (o1 == 0 && onSegment(p1, p2, q1)) return true; 
+
+    // p1, q1 and q2 are colinear and q2 lies on segment p1q1 
+    if (o2 == 0 && onSegment(p1, q2, q1)) return true; 
+
+    // p2, q2 and p1 are colinear and p1 lies on segment p2q2 
+    if (o3 == 0 && onSegment(p2, p1, q2)) return true; 
+
+    // p2, q2 and q1 are colinear and q1 lies on segment p2q2 
+    if (o4 == 0 && onSegment(p2, q1, q2)) return true; 
+
+    return false; // Doesn't fall in any of the above cases 
+} 
+
+function IsEdgeInsideExtent(extent,edge)
+{
+    if (IsNodeInsideExtent(extent, edge.source) || IsNodeInsideExtent(extent, edge.target))
+    {
+        return true;
+    }
+    else 
+    {
+        return DoesEdgeIntersectExtent(extent,edge);
+    }
+}
+
+function DoesEdgeIntersectExtent(extent,edge)
+{
+    rectangle = ConstructRectangleFromExtent(extent);
+    edgeSegment = new Segment(edge.source, edge.target);
+    doesIntersect = false;
+    count = 0;
+
+    while(doesIntersect == false && count < rectangle.length)
+    {
+        doesIntersect = doSegmentIntersect(rectangle[count],edgeSegment);
+        count += 1;
+    }
+
+    return doesIntersect;
 }
 
 function IsNodeInsideExtent(extent, node){
