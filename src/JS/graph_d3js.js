9cbaccb82d975f53f0142a88bcd3f53c43e6cfb0
Mathis Duban <mathis.duban@etu.umonpellier.fr>
2022-04-21 10:57:24 +0200

    user interface light mode information visualization solved

--- src/JS/graph_d3js.js
+++ src/JS/graph_d3js.js
@@ -1404,6 +1404,7 @@ function lightMode() {
     var allButton = document.getElementsByTagName("button");
     for (var j = 0, jmax = allButton.length; j < jmax; j++) {
         allButton[j].style.color = "white";
+        allButton[j].style.backgroundColor = "lightblue";
     }
     window.localStorage.setItem('themeSelect', 'lightMode');
 }
