eaab14f43d19bb0c0adbc9f4629dfc80dd1e6c62
Mathis Duban <mathis.duban@etu.umonpellier.fr>
2022-05-09 14:21:49 +0200

    rectification gith

--- src/JS/graph_d3js.js
+++ src/JS/graph_d3js.js
@@ -1347,12 +1347,6 @@ function DeleteAllEdgeGroups() {
     });
 }
 
-
-function UpdateG6Form(newg6) {
-    g6 = newg6;
-    document.querySelector('#g6').textContent = g6;
-}
-
 function checkIfExist(g) {
     window.open("https://hog.grinvin.org/DoSearchGraphFromGraph6String.action?graph6String=" + g6);
 }
